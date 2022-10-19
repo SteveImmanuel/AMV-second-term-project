a0c12e0b34e9c6d7a08e008858183452e7ba7dcc
SteveImmanuel <steveandreasimanuel@gmail.com>
2022-10-19 09:21:51 +0900

    Update training code

--- trainv2.py
+++ trainv2.py
@@ -5,7 +5,7 @@ from pytorch_lightning.loggers import TensorBoardLogger
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 from pytorch_lightning.callbacks.model_checkpoint import ModelCheckpoint
 from classifier import LitClassifier
-from dataset.extracted_dataset import KeypointExtractedDataset
+from dataset.extracted_dataset import KeypointExtractedDatasetV2
 from detectron_utils import *
 from constant import *
 
@@ -15,16 +15,16 @@ setup_dataset('semaphore_keypoint_train', 'data/train/annotation.json', 'data/tr
 setup_dataset('semaphore_keypoint_val', 'data/val/annotation.json', 'data/val')
 setup_dataset('semaphore_keypoint_test', 'data/test/annotation.json', 'data/test')
 
-train_dataset = KeypointExtractedDataset('semaphore_keypoint_train')
-val_dataset = KeypointExtractedDataset('semaphore_keypoint_val')
-test_dataset = KeypointExtractedDataset('semaphore_keypoint_test')
+train_dataset = KeypointExtractedDatasetV2('semaphore_keypoint_train', 'data/train/df2.csv')
+val_dataset = KeypointExtractedDatasetV2('semaphore_keypoint_val', 'data/val/df2.csv')
+test_dataset = KeypointExtractedDatasetV2('semaphore_keypoint_test', 'data/test/df2.csv')
 train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=NUM_WORKERS)
 val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=NUM_WORKERS)
 test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=NUM_WORKERS)
-classifier = LitClassifier(17 * 3, 28)
+classifier = LitClassifier(6 * 2, 28)
 
 logger = TensorBoardLogger(save_dir=os.getcwd(), name='runs')
-early_stop_cb = EarlyStopping(monitor='validation/loss', patience=100, verbose=True, mode='min')
+early_stop_cb = EarlyStopping(monitor='validation/loss', patience=200, verbose=True, mode='min')
 checkpoint_cb = ModelCheckpoint(monitor='validation/loss', save_top_k=1, mode='min')
 trainer = pl.Trainer(
     logger=logger,
@@ -34,7 +34,14 @@ trainer = pl.Trainer(
     log_every_n_steps=1,
     accelerator='gpu',
     devices=1,
-    callbacks=[early_stop_cb, checkpoint_cb],
+    # callbacks=[early_stop_cb, checkpoint_cb],
 )
 trainer.fit(classifier, train_loader, val_loader)
+
+# classifier = LitClassifier.load_from_checkpoint(
+#     checkpoint_cb.best_model_path,
+#     input_size=6 * 2,
+#     output_size=28,
+# )
+# classifier.eval()
 trainer.test(classifier, test_loader)
