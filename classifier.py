6afa663db847d3070d7b49be59f3e892050c7969
SteveImmanuel <steveandreasimanuel@gmail.com>
2022-10-19 09:21:25 +0900

    Update architecture

--- classifier.py
+++ classifier.py
@@ -1,4 +1,5 @@
 import os
+from typing import Tuple
 import torch
 import pytorch_lightning as pl
 from constant import *
@@ -11,6 +12,7 @@ class Classifier(torch.nn.Module):
             torch.nn.Linear(input_size, 128),
             torch.nn.ReLU(),
             torch.nn.BatchNorm1d(128),
+            torch.nn.Dropout(0.2),
             torch.nn.Linear(128, 64),
             torch.nn.ReLU(),
             torch.nn.BatchNorm1d(64),
@@ -19,6 +21,70 @@ class Classifier(torch.nn.Module):
 
     def forward(self, x):
         return self.model(x)
+
+
+class ClassifierV2(torch.nn.Module):
+    def __init__(self, input_size: int, output_size: int, conv_channel: int = 32):
+        super().__init__()
+        self.conv = torch.nn.Conv1d(2, conv_channel, 1, 1)
+        self.head = torch.nn.Sequential(
+            torch.nn.Linear((input_size // 2) * conv_channel, output_size),
+            # torch.nn.ReLU(),
+            # torch.nn.BatchNorm1d(64),
+            # torch.nn.Dropout(0.2),
+            # torch.nn.Linear(64, output_size),
+        )
+        self.embedding = torch.nn.Embedding(input_size // 2, conv_channel)
+        self.self_att = torch.nn.MultiheadAttention(conv_channel, 1)
+        self.layer_norm = torch.nn.LayerNorm(conv_channel)
+        self.summarization_token = self._get_weight((1, 1, conv_channel))
+
+    def _get_weight(self, size: Tuple):
+        weight = torch.empty(size, requires_grad=True)
+        weight = torch.nn.init.xavier_normal_(weight)
+        return torch.nn.Parameter(weight, requires_grad=True)
+
+    def forward(self, x: torch.tensor):
+        batch, n_features = x.shape
+
+        x = x.view(batch, 2, n_features // 2)
+        conv_out = self.conv(x).permute(0, 2, 1)
+        pos_embed = self.embedding(torch.arange(n_features // 2).cuda()).expand(batch, -1, -1)
+        seq = conv_out + pos_embed
+        att_out, _ = self.self_att(seq, seq, seq)  # (batch, n_features // 2, conv_channel)
+        out = self.layer_norm(seq + att_out)
+        out = out.flatten(1)  # (batch, n_features // 2 * conv_channel)
+        final_out = self.head(out)
+        return final_out
+
+
+class ClassifierV3(torch.nn.Module):
+    def __init__(self, input_size: int, output_size: int, conv_channel: int = 10):
+        super().__init__()
+        self.conv = torch.nn.Sequential(
+            torch.nn.Conv1d(2, conv_channel, 1, 1),
+            torch.nn.ReLU(),
+            torch.nn.Conv1d(conv_channel, conv_channel, 1, 1),
+            torch.nn.ReLU(),
+        )
+        self.head = torch.nn.Sequential(
+            torch.nn.Linear((input_size // 2) * conv_channel, 128),
+            torch.nn.ReLU(),
+            torch.nn.BatchNorm1d(128),
+            torch.nn.Dropout(0.2),
+            torch.nn.Linear(128, 64),
+            torch.nn.ReLU(),
+            torch.nn.BatchNorm1d(64),
+            torch.nn.Linear(64, output_size),
+        )
+
+    def forward(self, x: torch.tensor):
+        batch, n_features = x.shape
+
+        x = x.view(batch, 2, n_features // 2)
+        conv_out = self.conv(x).flatten(1)
+        final_out = self.head(conv_out)
+        return final_out
 
 
 class LitClassifier(pl.LightningModule):
