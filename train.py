9bdb80ebb75eef2e0a2ba0eb35e2e1f5af25af9e
SteveImmanuel <steveandreasimanuel@gmail.com>
2022-11-28 09:05:55 +0900

    Update training code

--- train.py
+++ train.py
@@ -1,26 +1,24 @@
 import logging
 import torch
+import os
 import pytorch_lightning as pl
 from pytorch_lightning.loggers import TensorBoardLogger
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 from pytorch_lightning.callbacks.model_checkpoint import ModelCheckpoint
-from classifier import LitClassifier, ClassifierV2, Classifier
-from dataset.extracted_dataset import KeypointExtractedDatasetV2
-from detectron_utils import *
+from models.lightning import LitClassifier
+from models.classifier import CNNClassifier
+from dataset.image_dataset import FER2013PlusDataset
 from constant import *
 
 logging.basicConfig(level=logging.INFO)
 
-setup_dataset('semaphore_keypoint_train', 'data/train/annotation.json', 'data/train')
-setup_dataset('semaphore_keypoint_val', 'data/val/annotation.json', 'data/val')
-
-train_dataset = KeypointExtractedDatasetV2('semaphore_keypoint_train', 'data/train/df2.csv')
-val_dataset = KeypointExtractedDatasetV2('semaphore_keypoint_val', 'data/val/df2.csv')
+train_dataset = FER2013PlusDataset('./data/fer2013plus/FER2013Train')
+val_dataset = FER2013PlusDataset('./data/fer2013plus/FER2013Valid')
 train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=NUM_WORKERS)
 val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=NUM_WORKERS)
-classifier = LitClassifier(6 * 2, 28, ClassifierV2)
+classifier = LitClassifier(CNNClassifier, train_dataset.total_class)
 
-logger = TensorBoardLogger(save_dir=os.getcwd(), name='runs', version='improved')
+logger = TensorBoardLogger(save_dir=os.getcwd(), name='runs')
 early_stop_cb = EarlyStopping(monitor='validation/acc', patience=100, verbose=True, mode='max')
 checkpoint_cb = ModelCheckpoint(monitor='validation/acc', save_top_k=1, mode='max')
 trainer = pl.Trainer(
@@ -28,7 +26,7 @@ trainer = pl.Trainer(
     max_epochs=MAX_EPOCHS,
     check_val_every_n_epoch=1,
     default_root_dir=os.getcwd(),
-    log_every_n_steps=1,
+    log_every_n_steps=5,
     accelerator='gpu',
     devices=1,
     callbacks=[early_stop_cb, checkpoint_cb],
