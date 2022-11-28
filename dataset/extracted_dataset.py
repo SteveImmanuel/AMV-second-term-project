02a74beb206570bcca39588d1b77101890f5cbd4
SteveImmanuel <steveandreasimanuel@gmail.com>
2022-11-28 09:05:38 +0900

    Update keypoint extraction pipeline

--- dataset/extracted_dataset.py
+++ dataset/extracted_dataset.py
@@ -2,62 +2,83 @@ import torch
 import pandas as pd
 import logging
 import os
-from detectron_utils import *
 from typing import Optional
 from tqdm import tqdm
+from models.keypoint_extractor import *
 
 
 class KeypointExtractedDataset(torch.utils.data.Dataset):
-    def __init__(self,
-                 dataset_name: str,
-                 cache_path: Optional[str] = None,
-                 model_extractor_config: Optional[str] = None):
-        self.dataset_name = dataset_name
-        self.split = dataset_name.split('_')[-1]
+    def __init__(
+        self,
+        dataset_path: str,
+        split: str,
+        cache_path: Optional[str] = None,
+        model_extractor: Optional[BaseKeypointExtractor] = None,
+    ):
+        self.dataset_path = dataset_path
+        self.split = split
         self.logger = logging.getLogger(__name__)
 
-        self.cache_path = cache_path if cache_path else f'./data/{self.split}/dataframe.csv'
+        self.cache_path = cache_path if cache_path else f'{dataset_path}/{split}.csv'
         if os.path.exists(self.cache_path):
             self.logger.info(f'Loading extracted keypoints from {self.cache_path}')
             self.dataframe = pd.read_csv(self.cache_path)
         else:
-            self.logger.info(f'No cache found for {self.dataset_name}, extracting keypoints')
-            self.model_extractor_config = model_extractor_config
+            self.logger.info(f'No cache found for {dataset_path}:{split}, extracting keypoints')
+            self._create_label_idx_mapping()
+            if not model_extractor:
+                self.model_extractor = MediapipeKeypointExtractor()
+            else:
+                self.model_extractor = model_extractor
             self._extract_keypoints()
 
-        self._create_label_idx_mapping()
-
     def _create_label_idx_mapping(self):
-        labels = self.dataframe['label'].unique()
+        labels = os.listdir(f'{self.dataset_path}/{self.split}')
         self.label_idx_mapping = {label: idx for idx, label in enumerate(labels)}
 
     def _extract_keypoints(self):
-        dataset = DatasetCatalog.get(self.dataset_name)
-        if self.model_extractor_config:
-            keypoint_cfg = get_model_config(self.model_extractor_config)
-        else:
-            keypoint_cfg = get_model_config()
-        keypoint_model = DefaultPredictor(keypoint_cfg)
-
-        columns = [[f'x{i}', f'y{i}', f'c{i}'] for i in range(17)]
-        columns = [*[item for sublist in columns for item in sublist], 'label']
+        total = 0
+        total_missing = 0
+        n_features = self.model_extractor.get_total_keypoints()
+        columns = [*[f'x{i}' for i in range(n_features)], 'label']
         data = {key: [] for key in columns}
-        for i in tqdm(range(len(dataset))):
-            label = os.path.splitext(os.path.split(dataset[i]['file_name'])[-1])[0]
-            keypoints = quick_predict_img(keypoint_model, dataset[i]['file_name'])
 
-            assert len(keypoints) >= 1, f'No keypoints found for {dataset[i]["file_name"]}'
-            if len(keypoints) > 1:
-                self.logger.warn(f'Multiple keypoints found for {dataset[i]["file_name"]}, using first one')
-            keypoints = keypoints[0].flatten()
+        base_path = os.path.join(self.dataset_path, self.split)
 
-            data['label'].append(label)
-            for j, key in enumerate(columns[:-1]):
-                data[key].append(keypoints[j].cpu().item())
+        for label in os.listdir(base_path):
+            self.logger.info(f'Extracting keypoints for label: {label}')
+            for img_name in tqdm(os.listdir(os.path.join(base_path, label))):
+                img_path = os.path.join(base_path, label, img_name)
+                img = cv2.imread(img_path)
+                keypoints = self.model_extractor.extract_keypoints(img)
+                data['label'].append(self.label_idx_mapping[label])
+                if keypoints:
+                    for i, keypoint in enumerate(keypoints):
+                        data[f'x{i}'].append(keypoint)
+                else:
+                    for i in range(n_features):
+                        data[f'x{i}'].append(0.0)
+                    self.logger.warn(f'Keypoints not found for image: {img_path}, defaulting to 0')
+                    total_missing += 1
+                total += 1
+
+        # for i in tqdm(range(len(dataset))):
+        #     label = os.path.splitext(os.path.split(dataset[i]['file_name'])[-1])[0]
+        #     keypoints = quick_predict_img(keypoint_model, dataset[i]['file_name'])
+
+        #     assert len(keypoints) >= 1, f'No keypoints found for {dataset[i]["file_name"]}'
+        #     if len(keypoints) > 1:
+        #         self.logger.warn(f'Multiple keypoints found for {dataset[i]["file_name"]}, using first one')
+        #     keypoints = keypoints[0].flatten()
+
+        #     data['label'].append(label)
+        #     for j, key in enumerate(columns[:-1]):
+        #         data[key].append(keypoints[j].cpu().item())
 
         self.dataframe = pd.DataFrame(data=data)
         self.logger.info(f'Saving extracted keypoints to {self.cache_path}')
         self.dataframe.to_csv(self.cache_path, index=False)
+        self.logger.info(f'Keypoints extracted for {total} images, {total_missing} missing')
 
     def __len__(self):
         return len(self.dataframe)
@@ -67,49 +88,5 @@ class KeypointExtractedDataset(torch.utils.data.Dataset):
         return torch.FloatTensor(item[:-1]), torch.LongTensor([self.label_idx_mapping[item[-1]]]).squeeze()
 
 
-class KeypointExtractedDatasetV2(KeypointExtractedDataset):
-    def __init__(self,
-                 dataset_name: str,
-                 cache_path: Optional[str] = None,
-                 model_extractor_config: Optional[str] = None):
-        super().__init__(dataset_name, cache_path, model_extractor_config)
-
-    def _extract_keypoints(self):
-        dataset = DatasetCatalog.get(self.dataset_name)
-        if self.model_extractor_config:
-            keypoint_cfg = get_model_config(self.model_extractor_config)
-        else:
-            keypoint_cfg = get_model_config()
-        keypoint_model = DefaultPredictor(keypoint_cfg)
-
-        columns = [[f'x{i}', f'y{i}'] for i in range(17)]
-        columns = [*[item for sublist in columns for item in sublist], 'label']
-        data = {key: [] for key in columns}
-        for i in tqdm(range(len(dataset))):
-            label = os.path.splitext(os.path.split(dataset[i]['file_name'])[-1])[0]
-            keypoints = quick_predict_img(keypoint_model, dataset[i]['file_name'])
-
-            assert len(keypoints) >= 1, f'No keypoints found for {dataset[i]["file_name"]}'
-            if len(keypoints) > 1:
-                self.logger.warn(f'Multiple keypoints found for {dataset[i]["file_name"]}, using first one')
-            keypoints = keypoints[0].flatten()
-
-            data['label'].append(label)
-            for j in range(17):
-                x = keypoints[3 * j].cpu().item() / dataset[i]['width']
-                y = keypoints[3 * j + 1].cpu().item() / dataset[i]['height']
-                data[f'x{j}'].append(x)
-                data[f'y{j}'].append(y)
-
-        for i in range(17):
-            if i not in [5, 6, 7, 8, 9, 10]:
-                del data[f'x{i}']
-                del data[f'y{i}']
-
-        self.dataframe = pd.DataFrame(data=data)
-        self.logger.info(f'Saving extracted keypoints to {self.cache_path}')
-        self.dataframe.to_csv(self.cache_path, index=False)
-
-    def __getitem__(self, idx: int):
-        item = list(self.dataframe.iloc[idx])
-        return torch.FloatTensor(item[:-1]), torch.LongTensor([self.label_idx_mapping[item[-1]]]).squeeze()
+if __name__ == '__main__':
+    fer2013keypoints = KeypointExtractedDataset('data/fer2013', 'train')
